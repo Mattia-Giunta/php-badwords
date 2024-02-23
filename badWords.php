f5d33bc0d1a2cd3e8058e45652436cca840d45c7
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-02-23 13:15:01 +0100

    creato repo e eseguito esercizio

---
+++ badWords.php
@@ -0,0 +1,49 @@
+
+
+
+<?php 
+    // variabile che prende tramite GET e il name il testo inserito
+    $testo_inserito = $_GET['paragrafo'];
+
+    // variabile che prende tramite GET e il name la parola da censurare
+    $parola_da_censurare = $_GET['censura'];
+
+    // variabile che prende la parola da censurare e il testo inserito e gli applica la censura
+    $testo_censurato = str_replace($parola_da_censurare, 'xxx' ,$testo_inserito);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>DBadWors Result</title>
+
+</head>
+
+<body>
+
+    <!-- testo intero con controllo della lunghezza -->
+    <h1>Il testo che hai inserito è:</h1>
+
+    <p><?php echo $testo_inserito ?></p>
+
+    <p>e ha una lunghezza di <?php echo strlen($testo_inserito);  ?></p>
+
+
+    <!-- testo censurtato con coltrollo della lunghezza -->
+    <h1>Il testo con la censura è:</h1>
+
+    <p><?php echo $testo_censurato ?></p>
+        
+    <p>e ha una lunghezza di <?php echo strlen($testo_censurato);  ?></p>   
+        
+</body>
+</html>
+
+<!-- terminato esercizio -->
